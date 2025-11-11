5244dfd10db66d16f315f8dbdc1be24728231744
dn3222151-ctrl <dn3222151@gmail.com>
2025-11-11 23:10:18 +0700

    test 1

---
+++ src/main/java/controller/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+/**
+ *
+ * @author dn322
+ */
+public class NewClass {
+    private int soluong;
+    
+}
